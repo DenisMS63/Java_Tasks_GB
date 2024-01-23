8a5fab03cc55b6de141f472f172a2e315343cee9
Denis <moiseev-denis@mail.ru>
2024-01-23 10:40:24 +0400

    Решили задачу 2 дз

--- Test.java
+++ Test.java
@@ -1,16 +1,24 @@
+
+
 public class Test {
     public static void main(String[] args) {
 
-        for (int i = 2; i <= 15; i++) {
+        int count = 0;                          // вывести все простые числа от 1 до 1000
 
-            for (int j = 2; j <= i; j++) {
+        for (int i = 1; i < 1000; i++) {            
+            
+            count = 0;
 
+            for (int j = 1; j <= i; j++) {
+                
                 if (i % j == 0) {
-
-                    System.out.println(i);
-
-                } 
+                    count++;
+                }
             }
-        }
-    }
+            if (count == 2) {
+                System.out.println(i);
+            }
+        }  
+        
+    } 
 }
