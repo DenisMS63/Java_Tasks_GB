3535b9fda0a6cd33ca6b30adf1fd6ce5eee8d87f
Denis <moiseev-denis@mail.ru>
2024-01-25 15:21:23 +0400

    Решил задачу 3 - сем 2

--- Seminar/Sem_2_task_3.java
+++ Seminar/Sem_2_task_3.java
@@ -1,5 +1,34 @@
+/*
+ * Задание №3
+Напишите метод, который принимает на вход строку (String) и
+определяет является ли строка палиндромом (возвращает
+boolean значение).
+ */
+
 package Seminar;
 
 public class Sem_2_task_3 {
-    
-}
+    public static void main(String[] args) {
+        
+        String str = "А роза упала на лапу Азора";
+
+        System.out.println(polindrom (str));
+        
+
+    }
+
+    static boolean polindrom (String s) {
+
+        s = s.toLowerCase();
+        s = s.replace(" ", "");
+        
+        
+        for (int i = 0; i < s.length() / 2; i++) {
+            
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {return false;}
+            
+        }
+
+        return true;
+    }
+}   
